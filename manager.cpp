fe454703d19767bb6f46538d38c373ec15b448f1
ALI AKBAR <50044603+ALI1111111111@users.noreply.github.com>
2023-01-15 09:03:06 +0500

    defining functions

---
+++ manager.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include< string.h>
+#include< fstream>
+using namespace std;
+
+
+void acc_create(int acc_id,string customer_name,float opening_balance);
+void acc_information(int acc_id);
+void deposit(float dep_money);
+void withdraw(float wd_money);
+void transfer_money(int cus_payee_id,int cus_acc_id);
+void acc_close(int cus_acc_id);
+void cus_details(int cus_acc_id);
+
+
+int main() {
+
+
+	return 0;
+}
